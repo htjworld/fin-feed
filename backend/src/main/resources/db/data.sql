b50f7150c9176f4d63f633b64e74b5c74d949f87
htjworld <116538001+htjworld@users.noreply.github.com>
2026-05-22 00:33:27 +0900

    feat: 핀테크 회사 12개 추가 (Nubank, Monzo, N26, Robinhood, Block, Brex, Coinbase, 핀다 등)

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -19,9 +19,19 @@ INSERT INTO companies (name, name_en, rss_url, site_url, sector, crawl_type, is_
 ('클레이튼', 'Klaytn', 'https://blog.kaia.io/feed/', 'https://blog.kaia.io', 'crypto', 'RSS', true, '/logos/klaytn.png'),
 
 -- 해외 핀테크
-('Stripe',  'Stripe',  'https://stripe.com/blog/feed.rss',         'https://stripe.com/blog/engineering',  'global_fintech', 'RSS', true, '/logos/stripe.svg'),
-('Plaid',   'Plaid',   'https://plaid.com/blog/rss.xml',           'https://plaid.com/blog',               'global_fintech', 'RSS', true, '/logos/plaid.png'),
-('Revolut', 'Revolut', 'https://medium.com/feed/revolut',          'https://medium.com/revolut',           'global_fintech', 'RSS', true, '/logos/revolut.png'),
-('Wise',    'Wise',    'https://medium.com/feed/wise-engineering',  'https://medium.com/wise-engineering',  'global_fintech', 'RSS', true, '/logos/wise.png')
+('Stripe',   'Stripe',   'https://stripe.com/blog/feed.rss',                 'https://stripe.com/blog/engineering',      'global_fintech', 'RSS', true, '/logos/stripe.svg'),
+('Plaid',    'Plaid',    'https://plaid.com/blog/rss.xml',                   'https://plaid.com/blog',                   'global_fintech', 'RSS', true, '/logos/plaid.png'),
+('Revolut',  'Revolut',  'https://medium.com/feed/revolut',                  'https://medium.com/revolut',               'global_fintech', 'RSS', true, '/logos/revolut.png'),
+('Wise',     'Wise',     'https://medium.com/feed/wise-engineering',          'https://medium.com/wise-engineering',      'global_fintech', 'RSS', true, '/logos/wise.png'),
+('Nubank',   'Nubank',   'https://building.nubank.com.br/feed/',              'https://building.nubank.com.br',           'global_fintech', 'RSS', true, '/logos/nubank.svg'),
+('Monzo',    'Monzo',    'https://medium.com/feed/monzo-bank',                'https://monzo.com/blog',                   'global_fintech', 'RSS', true, '/logos/monzo.svg'),
+('N26',      'N26',      'https://medium.com/feed/n26',                       'https://medium.com/n26',                   'global_fintech', 'RSS', true, '/logos/n26.svg'),
+('Robinhood','Robinhood','https://medium.com/feed/robinhood-engineering',     'https://medium.com/robinhood-engineering',  'global_fintech', 'RSS', true, '/logos/robinhood.svg'),
+('Block',    'Block',    'https://developer.squareup.com/blog/feed/',         'https://developer.squareup.com/blog',      'global_fintech', 'RSS', true, '/logos/block.svg'),
+('Brex',     'Brex',     'https://medium.com/feed/brex-tech',                 'https://medium.com/brex-tech',             'global_fintech', 'RSS', true, '/logos/brex.svg'),
+('Coinbase', 'Coinbase', 'https://blog.coinbase.com/feed',                   'https://blog.coinbase.com',                'crypto',         'RSS', true, '/logos/coinbase.svg'),
+
+-- 국내 핀테크 추가
+('핀다', 'Finda', 'https://medium.com/feed/finda-tech', 'https://medium.com/finda-tech', 'domestic_fintech', 'RSS', true, '/logos/finda.svg')
 
 ON CONFLICT (name_en) DO NOTHING;
